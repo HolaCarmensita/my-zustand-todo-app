8cbaff9fe8a0e0498cc2941be3dad21de34eb8ae
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2025-05-21 09:26:22 +0200

    project structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+  <header
+    style={{ padding: '1rem', background: '#f5f5f5', textAlign: 'center' }}
+  >
+    <h1>My Zustand Todo App</h1>
+  </header>
+);
+
+export default Header;
