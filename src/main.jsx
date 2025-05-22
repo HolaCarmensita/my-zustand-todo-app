d92d4de6df1b891508941fb3d01155c6a1e8c574
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2025-05-22 08:23:45 +0200

    tested the store in the console

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App.jsx';
+import './index.css';
+import useTaskStore from './store/useTaskStore'; // ← importera din store-hook
 
-import { App } from './App.jsx'
-
-import './index.css'
+window.taskStore = useTaskStore;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
